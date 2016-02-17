6ece6ae50025d97e891e0c3cc459712e908722cc
Cesidio Di Landa <cesidio.dilanda@gmail.com>
2016-02-17 07:26:54 +0000

    Add PrisonNumber module and shared example
    This module adds a prison_number attribute with field validation
    on the format.

--- spec/forms/search_prisoner_spec.rb
+++ spec/forms/search_prisoner_spec.rb
@@ -3,17 +3,7 @@ require 'rails_helper'
 RSpec.describe SearchPrisoner, type: :form do
   subject { described_class.new(prison_number: 'A1234BC') }
 
-  context 'validations' do
-    context 'prison_number' do
-      context 'with valid format' do
-        it { is_expected.to be_valid }
-      end
-      context 'with invalid format' do
-        subject { described_class.new(prison_number: 'invalid') }
-        it { is_expected.to_not be_valid }
-      end
-    end
-  end
+  it_behaves_like 'a form with prison_number'
 
   describe '#results?' do
     context 'when results exist' do
